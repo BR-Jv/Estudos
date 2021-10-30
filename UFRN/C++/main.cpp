38715eb1bb5ad7f2d54afef9a2443016fbbcabd5
João.Victor <jv.alves193@gmail.com>
2021-10-29 22:00:07 -0300

    atualização

--- UFRN/C++/main.cpp
+++ UFRN/C++/main.cpp
@@ -3,8 +3,23 @@
 using namespace std;
 
 int main(){
-   int x = 5, y = 7;
-   x = x++ + ++x;
-   y += -x;
-   cout<<"x: "<<x<<"y: "<<y;
+   int x, y , z, t;
+   cin >> x >> y >> z;
+   if(x < y){ // 12 < 3 
+      t = x; //
+      x = y; //
+      y = t; //
+   }
+   if(x < z){ //12 < 15
+      t = x; // t = 12
+      x = z; // x = 15
+      z = t; // z = 12
+   }
+   if(y < z){ //3<15
+      t = y; //t = 3
+      y = z; //y = 12
+      z = t; //z = 3
+   }
+   cout << x << " " << y << " " << z << endl;
+   return 0;
 }
