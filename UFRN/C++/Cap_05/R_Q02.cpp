098bc9d845bbd9dc4e81b921fb03641df96983e8
João.Victor <jv.alves193@gmail.com>
2021-11-21 21:42:22 -0300

    att

--- UFRN/C++/Cap_05/R_Q02.cpp
+++ UFRN/C++/Cap_05/R_Q02.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
+char muda_caso (char x);
+
 int main(){
-   
+   char c;
+   cout << "Digite um caractere: " << endl;
+   cin >> c;
+   cout << "O caractere digitado foi: " << c << endl;
+   cout << "O caractere digitado alterado: " <<  muda_caso(c) << endl;
    return 0;
 }
+
+
+char muda_caso (char c){
+   int num_char = int(c);
+   
+   if (num_char > 90){
+      num_char -= 32;
+   }else {
+      num_char += 32;
+   }
+   
+   return (char)num_char;
+  
+}
+
