41a7e85587c9170e15e6c4785c491ef1a7019f0c
João.Victor <jv.alves193@gmail.com>
2021-11-28 15:15:58 -0300

    DOM att

---
+++ Estudos/Html_CSS_JS/Estudo_DOM/02/scripts.js
@@ -0,0 +1,10 @@
+let input_color = document.getElementById('input_color');
+let div = document.getElementById('div');
+/*
+
+div.style.backgroundColor = input_color.value;
+*/
+
+input_color.addEventListener('input', (event) =>{
+   div.style.backgroundColor = event.target.value;
+}, false);
--- UFRN/C++/main.cpp
+++ UFRN/C++/main.cpp
@@ -7,12 +7,8 @@ int main(){
    int numero, inverso, u, d, c, cm;
    
    cin >> numero;
-   u = numero%10; 
-   d = (numero - u) / 10 % 10;
-   c = ((numero - u) / 10 - d)/10%10;  
-   cm = ((numero - u) / 10 - d) / 10;
    
-
+   
    cout << "Numero: " << numero << endl;
 
    inverso = u*1000 + d*100 + c*10 + cm;
