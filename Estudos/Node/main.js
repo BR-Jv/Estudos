41617438848968cf92c5187c887682200b5cf96a
João Alves <jv.alves193@gmail.com>
2022-03-17 23:42:07 -0300

    Refazendo o projeto do zero

--- Estudos/Node/main.js
+++ Estudos/Node/main.js
@@ -1,4 +1,2 @@
 
-const myArray = [0, 1, 2, 3, 4, 5];
-var n = 2;
 
--- UFRN/C++/main.cpp
+++ UFRN/C++/main.cpp
@@ -2,40 +2,7 @@
 
 using namespace std;
 
-struct Produto {
-  int codigo;
-  double valor;
-};
-
-Produto maisCaro(Produto VP[], int n);
-
-int main(){
-  int n;
-  cin >> n;
-  Produto VP[n];
-  for(int i = 0;i<n;i++){
-    cin >> VP[i].codigo; 
-    cin >> VP[i].valor;
-  }
-
-  Produto MC = maisCaro(VP, n);
-  
-  cout << MC.codigo << endl;
-  cout << MC.valor;
+int main() {
   
   return 0;
 }
-
-Produto maisCaro(Produto VP[], int n){
-  Produto mais_caro;
-  mais_caro.valor = 0;
-
-  for(int j= 0;j < n;j++){
-    if(VP[j].valor > mais_caro.valor){
-      mais_caro.valor = VP[j].valor;
-      mais_caro.codigo = VP[j].codigo;
-    }
-  }
-
-  return mais_caro;
-};
