078c113d7b8923edde5712f4eb14a49b8186ec96
João Alves <jv.alves193@gmail.com>
2022-07-19 23:50:49 -0300

    add validação e variavel especial

--- Estudos/C++/Questão07.cpp
+++ Estudos/C++/Questão07.cpp
@@ -11,14 +11,16 @@ struct Carro {
    Horario entrada, saida;
 };
 
-void InputValues(Carro obj[], int index);
-
 void OutputValues(Carro obj[], int index);
+
+void HorarioEntrada(Carro obj[], int index,bool &key);
+
+void HorarioSaida(Carro obj[], int index);
 
 int main(){
    int n;
+   bool key = true; //Usada para saber se a função HorarioSaida e outputvalues devem ser executados;
    
-   //Quantidade de elementos do vetor db
    cout << "Digite a quantidade de elementos do vetor db: " << endl;
    cin >> n;
    
@@ -26,31 +28,88 @@ int main(){
 
    //Preenchimento do vetor
    for(int i = 0; i < n; i++){
-      InputValues(db, i);
+      HorarioEntrada(db, i, key);
+      if(key){
+         HorarioSaida(db, i);
+      }else{
+         cout << "Algo deu errado no horario de entrada.";
+      }
    }
 
    //Visualizando os elementos do vetor
-   for(int j = 0; j < n; j++){
-      OutputValues(db, j);
-   }
+   if(key){
+      for(int j = 0; j < n; j++){
+         OutputValues(db, j);
+      }
+   }else {
+      cout << " Programa finalizado antes do previsto.";
+   };
    
    return 0;
 }
 
+void HorarioSaida(Carro obj[], int index){
+   int hora, min, seg;
 
-void InputValues(Carro obj[], int index){
+   cout << "Digite o horario(no formato, hh-mm-ss) de saida do carro: " << endl;
+   
+   //Iniciei a validação por hora.
+   cin >> hora;
+   if(hora >= 0 && hora <= 23){
+      obj[index].saida.hora = hora;
+      //Sendo hora um valor válído, eu faço a validação do minuto em seguida.
+      cin >> min;
+      if(min >= 0 && min <= 59){
+         obj[index].saida.min = min;
+         //Seguindo a lógica anterior, o próximo passo é validar os segundos.
+         cin >> seg;
+         if(seg >= 0 && seg <= 59){
+            obj[index].saida.seg = seg;
+         }else {
+            cout << "Erro, hora inválido.";
+         }
+      }else {
+         cout << "Erro, hora inválido.";
+      }
+   }else {
+      cout << "Erro, hora inválido.";
+   }
+}
+
+//Segue a mesma lógica de HorarioSaida()
+void HorarioEntrada(Carro obj[], int index,bool &key){
+   int hora, min, seg;
+
    cout << "Digite a placa do carro: " << endl;
    cin >> obj[index].Placa;
-   
-   //! Tenho que fazer uma validação nesse campo 
-   cout << "Digite o horario(hh-mm-ss) de entrada do carro: " << endl;
-   cin >> obj[index].entrada.hora >> obj[index].entrada.min >> obj[index].entrada.seg;
-   
-   //! Tenho que fazer uma validação nesse campo
-   cout << "Digite o horario(hh-mm-ss) de saida do carro: " << endl;
-   cin >> obj[index].saida.hora >> obj[index].saida.min >> obj[index].saida.seg;
 
-};
+   cout << "Digite o horario(no formato, hh-mm-ss) de entrada do carro: " << endl;
+   
+   //Iniciei a validação por hora.
+   cin >> hora;
+   if(hora >= 0 && hora <= 23){
+      obj[index].entrada.hora = hora;
+      //Sendo hora um valor válído, eu faço a validação do minuto em seguida.
+      cin >> min;
+      if(min >= 0 && min <= 59){
+         obj[index].entrada.min = min;
+         //Seguindo a lógica anterior, o próximo passo é validar os segundos.
+         cin >> seg;
+         if(seg >= 0 && seg <= 59){
+            obj[index].entrada.seg = seg;
+         }else {
+            cout << "Erro, hora invalido."<< endl;
+            key = false;
+         }
+      }else {
+         cout << "Erro, hora invalido."  << endl;
+         key = false;
+      }
+   }else {
+      cout << "Erro, hora invalido."<< endl;
+      key = false;
+   }
+}
 
 void OutputValues(Carro obj[], int index){
    cout << "Placa do carro: "<< obj[index].Placa << endl ;
